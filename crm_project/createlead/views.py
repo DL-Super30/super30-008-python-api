e045244bf5aa7210ed03ccd6f02d6dc5d5c27770
Varugk11 <162870373+Varugk11@users.noreply.github.com>
2024-09-21 11:55:03 +0530

    Update views.py

--- crm_project/createlead/views.py
+++ crm_project/createlead/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 
 
 from .models import CreateLeads
@@ -10,3 +12,7 @@ class CreateLeadsViewSet(viewsets.ModelViewSet):
 
     queryset = CreateLeads.objects.all()
     serializer_class = CreateLeadsSerializer
+    def destroy(self, request, pk=None):
+        instance = self.get_object()
+        instance.delete()
+        return Response({'message': 'Record Deleted Successfully. '}, status=HTTP_204_NO_CONTENT)
